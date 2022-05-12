fb99b568de1587d04e1495d5090f26d2f9c23579
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-05-12 15:25:10 +0200

    create function index() + retrieve data from the database

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('movies.index', );
+        return view('movies.index',  ['movies' => Movie::all()]);
     }
 
 }
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,1 +1,10 @@
 @extends('layouts.main')
+
+
+<div>
+    @foreach ($movies as $movie)
+
+    <p>{{$movie->title}}</p>
+
+    @endforeach
+</div>
